1d6c2349d5d0a7324d4c4a4deb5cf812f47327d2
devxn <dgomez12@ggc.edu>
2025-10-23 21:53:47 -0400

    Added exercise command to 03-happy.sh

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,11 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+day=$(date +%A)
+
+if [ "$day" = "Saturday"] || [ "$day" = "Sunday" ]; then
+	echo "It's the weekend!"
+else 
+	echo "It's the weekday!"
+fi
+
